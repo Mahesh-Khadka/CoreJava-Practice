ea8296a555985f2e9fee4f2acbddeb17ece0b772
Mahesh-Khadka <maheshkhadka438@gmail.com>
2025-10-14 20:04:56 +0545

    implement Method Overriding that demonstrate run time polymorphism

---
+++ src/Phase1_OOP/Method_Overriding.java
@@ -0,0 +1,18 @@
+package Phase1_OOP;
+class Cricket{
+    void player(){
+        System.out.println("Virat is a king.");
+    }
+}
+class India extends Cricket{
+    void player(){
+        super.player();
+        System.out.println("Dhoni is a GOAT.");
+    }
+}
+public class Method_Overriding {
+    public static void main(String[] args){
+        India i = new India();
+        i.player();
+    }
+}
