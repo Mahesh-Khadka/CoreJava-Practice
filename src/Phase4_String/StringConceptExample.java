b363edaa7a886ff92fdc1568498dfafc6fcbd6cb
Mahesh-Khadka <maheshkhadka438@gmail.com>
2025-11-09 19:10:22 +0545

    feat(Phase3_String): Implement the concept of String, StringBuffer, StringBuilder

---
+++ src/Phase4_String/StringConceptExample.java
@@ -0,0 +1,25 @@
+package Phase4_String;
+
+public class StringConceptExample {
+    public static void main(String[] args){
+        String s1 = "Mahesh";
+        String s2 = "MaHesh";
+        System.out.println(s1 == s2);
+        System.out.println(s1.equalsIgnoreCase(s2));
+
+        String s3 = new String("Mahesh");
+        String s4 = new String("Mahesh");
+        System.out.println(s3 == s4);
+
+        StringBuilder sb1 = new StringBuilder("Ram");
+        sb1.append(" Thapa");
+        sb1.insert(3, " Bahadur");
+        System.out.println(sb1);
+        String s5 = "Ram Bahadur Thapa";
+        System.out.println(s5.equalsIgnoreCase(sb1.toString()));
+
+        StringBuffer sb2 = new StringBuffer("Krishna");
+        sb2.append(" Khadka");
+        System.out.println("sb2");
+    }
+}
